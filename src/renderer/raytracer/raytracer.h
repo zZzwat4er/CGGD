635204b849da940ba3ee8607182035611b78de28
zZzwat4er <valenten2001@mail.ru>
2022-10-20 21:11:37 +0300

    1.01

--- src/renderer/raytracer/raytracer.h
+++ src/renderer/raytracer/raytracer.h
@@ -122,14 +122,15 @@ namespace cg::renderer
 	inline void raytracer<VB, RT>::set_render_target(
 			std::shared_ptr<resource<RT>> in_render_target)
 	{
-		// TODO Lab: 2.01 Implement `set_render_target`, `set_viewport`, and `clear_render_target` methods of `raytracer` class
+		render_target = in_render_target;
 	}
 
 	template<typename VB, typename RT>
 	inline void raytracer<VB, RT>::set_viewport(size_t in_width,
 												size_t in_height)
 	{
-		// TODO Lab: 2.01 Implement `set_render_target`, `set_viewport`, and `clear_render_target` methods of `raytracer` class
+		width = in_width;
+		height = in_height;
 		// TODO Lab: 2.06 Add `history` resource in `raytracer` class
 	}
 
@@ -137,7 +138,10 @@ namespace cg::renderer
 	inline void raytracer<VB, RT>::clear_render_target(
 			const RT& in_clear_value)
 	{
-		// TODO Lab: 2.01 Implement `set_render_target`, `set_viewport`, and `clear_render_target` methods of `raytracer` class
+		for(size_t i = 0; i < render_target->get_number_of_elements(); i++)
+		{
+			render_target->item(i) = in_clear_value;
+		}
 		// TODO Lab: 2.06 Add `history` resource in `raytracer` class
 	}
 
