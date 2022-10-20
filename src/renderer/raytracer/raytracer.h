23e7868cf36f44ad46544f2eb91738282531619b
zZzwat4er <valenten2001@mail.ru>
2022-10-20 23:45:00 +0300

    1.14

--- src/renderer/raytracer/raytracer.h
+++ src/renderer/raytracer/raytracer.h
@@ -208,6 +208,24 @@ namespace cg::renderer
 		if (depth == 0) return miss_shader(ray);
 		depth--;
 
+		payload closest_hit_payload{};
+		closest_hit_payload.t = max_t;
+		const triangle<VB>* closest_triangle = nullptr;
+
+		for(auto & triangle : triangles){
+			payload payload = intersection_shader(triangle, ray);
+			if(payload.t > min_t && payload.t < closest_hit_payload.t)
+			{
+				closest_hit_payload = payload;
+				closest_triangle = &triangle;
+			}
+		}
+
+		if(closest_hit_payload.t < max_t)
+		{
+			if(closest_hit_shader) return closest_hit_shader(ray, closest_hit_payload, *closest_triangle, depth);
+		}
+
 		return miss_shader(ray);
 		// TODO Lab: 2.02 Adjust `trace_ray` method of `raytracer` class to traverse geometry and call a closest hit shader
 		// TODO Lab: 2.04 Adjust `trace_ray` method of `raytracer` to use `any_hit_shader`
