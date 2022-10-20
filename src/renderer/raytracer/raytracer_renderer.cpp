06adfb92074621f0fc9c07645981076d820ddde9
zZzwat4er <valenten2001@mail.ru>
2022-10-20 21:24:17 +0300

    1.02

--- src/renderer/raytracer/raytracer_renderer.cpp
+++ src/renderer/raytracer/raytracer_renderer.cpp
@@ -7,7 +7,34 @@
 
 void cg::renderer::ray_tracing_renderer::init()
 {
-	// TODO Lab: 2.01 Add `render_target`, `camera`, and `raytracer` in `ray_tracing_renderer` class
+	model = std::make_shared<cg::world::model>();
+	model->load_obj(settings->model_path);
+
+	camera = std::make_shared<cg::world::camera>();
+	camera->set_height(static_cast<float>(settings->height));
+	camera->set_width(static_cast<float>(settings->width));
+
+	camera->set_position(float3{
+			settings->camera_position[0],
+			settings->camera_position[1],
+			settings->camera_position[2]
+	});
+	camera->set_phi(settings->camera_phi);
+	camera->set_theta(settings->camera_theta);
+	camera->set_angle_of_view(settings->camera_angle_of_view);
+	camera->set_z_near(settings->camera_z_near);
+	camera->set_z_far(settings->camera_z_far);
+
+	render_target = std::make_shared<cg::resource<cg::unsigned_color>>(settings->width, settings->height);
+
+	raytracer = std::make_shared<cg::renderer::raytracer<cg::vertex, cg::unsigned_color>>();
+	raytracer->set_render_target(render_target);
+	raytracer->set_viewport(settings->width, settings->height);
+	raytracer->set_vertex_buffers(model->get_vertex_buffers());
+	raytracer->set_index_buffers(model->get_index_buffers());
+
+
+
 	// TODO Lab: 2.03 Add light information to `lights` array of `ray_tracing_renderer`
 	// TODO Lab: 2.04 Initialize `shadow_raytracer` in `ray_tracing_renderer`
 }
