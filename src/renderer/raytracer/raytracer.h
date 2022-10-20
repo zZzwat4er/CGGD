bc00ef192b28e9e655b6ede3e974c3feb568e4f3
zZzwat4er <valenten2001@mail.ru>
2022-10-21 00:40:25 +0300

    1.21

--- src/renderer/raytracer/raytracer.h
+++ src/renderer/raytracer/raytracer.h
@@ -232,6 +232,7 @@ namespace cg::renderer
 			{
 				closest_hit_payload = payload;
 				closest_triangle = &triangle;
+				if(any_hit_shader) return any_hit_shader(ray, payload, triangle);
 			}
 		}
 
@@ -241,8 +242,6 @@ namespace cg::renderer
 		}
 
 		return miss_shader(ray);
-		// TODO Lab: 2.02 Adjust `trace_ray` method of `raytracer` class to traverse geometry and call a closest hit shader
-		// TODO Lab: 2.04 Adjust `trace_ray` method of `raytracer` to use `any_hit_shader`
 		// TODO Lab: 2.05 Adjust `trace_ray` method of `raytracer` class to traverse the acceleration structure
 	}
 
